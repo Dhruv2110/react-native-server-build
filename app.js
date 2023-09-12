4f0daf7aaf1a178088216aef3f99162ab3725cf3
dev_dhruv <dhruv.s@codebrewinnovations.com>
2023-09-12 17:50:42 +0530

    test

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/test", (req, res) =>  {
-  res.status(200).send("Hello world")
-})
+// app.get("/test", (req, res) =>  {
+//   res.status(200).send("Hello world")
+// })
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log('Example app listening on port 3000!'),
-);
+// app.listen(port, () =>
+//   console.log('Example app listening on port 3000!'),
+// );
+console.log('Hello world');
